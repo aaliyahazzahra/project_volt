5d55308b1f1f5335d55d2cf5b5292f146a6d0806
aaliyahazzahra <aariariaa02@gmail.com>
2025-11-17 06:43:48 +0700

    authenticator

--- lib/features/1_auth/authenticator.dart
+++ lib/features/1_auth/authenticator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animated_gradients/flutter_animated_gradients.dart';
 import 'package:project_volt/core/constants/app_image.dart';
 import 'package:project_volt/features/1_auth/login_form.dart';
 import 'package:project_volt/features/1_auth/register_form.dart';
@@ -11,60 +12,89 @@ class Authenticator extends StatelessWidget {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-        body: SingleChildScrollView(
-          padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 60.0),
-          child: Column(
-            children: [
-              // Bagian Header
-              SizedBox(height: 16),
-              Image.asset(AppImages.volt, height: 100),
-
-              SizedBox(height: 40),
-
-              // Bagian TabBar
-              Container(
-                height: 50,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(25.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.1),
-                      blurRadius: 10,
-                      offset: Offset(0, 5),
-                    ),
-                  ],
-                ),
-                child: TabBar(
-                  indicator: BoxDecoration(
-                    color: Colors.blueAccent,
-                    borderRadius: BorderRadius.circular(25.0),
-                  ),
-                  indicatorSize: TabBarIndicatorSize.tab,
-
-                  labelColor: Colors.white,
-                  unselectedLabelColor: Colors.grey,
-                  tabs: [
-                    Tab(text: 'Login'),
-                    Tab(text: 'Registrasi'),
-                  ],
-                ),
-              ),
-
-              // Bagian Form
-              SizedBox(
-                height: 500,
-                child: TabBarView(
-                  children: [
-                    // Halaman 1: Form Login
-                    LoginForm(),
-
-                    // Halaman 2: Form Registrasi
-                    RegisterForm(),
-                  ],
-                ),
-              ),
+        body: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: AnimatedGradientBackground(
+            colors: const [
+              Color(0xffc4c9f2),
+              Color.fromARGB(255, 158, 204, 231),
+              Color.fromARGB(255, 240, 175, 90),
+              Color(0xffffc22f),
+              Color.fromARGB(255, 240, 175, 90),
+              Color.fromARGB(255, 158, 204, 231),
+              Color(0xffc4c9f2),
             ],
+            duration: const Duration(seconds: 10),
+
+            child: SingleChildScrollView(
+              padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 60.0),
+              child: Column(
+                children: [
+                  // Bagian Header
+                  SizedBox(height: 16),
+                  Image.asset(AppImages.volt, height: 100),
+
+                  SizedBox(height: 40),
+
+                  // Bagian TabBar
+                  Container(
+                    height: double.minPositive,
+                    width: double.minPositive,
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.5),
+                      border: Border.all(color: Colors.white.withOpacity(0.7)),
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    child: Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(25.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.1),
+                            blurRadius: 10,
+                            offset: Offset(0, 5),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        children: [
+                          TabBar(
+                            indicator: BoxDecoration(
+                              color: Colors.blueAccent,
+                              borderRadius: BorderRadius.circular(25.0),
+                            ),
+                            indicatorSize: TabBarIndicatorSize.tab,
+
+                            labelColor: Colors.white,
+                            unselectedLabelColor: Colors.grey,
+                            tabs: [
+                              Tab(text: 'Login'),
+                              Tab(text: 'Registrasi'),
+                            ],
+                          ),
+                          // Bagian Form
+                          SizedBox(
+                            height: 500,
+                            child: TabBarView(
+                              children: [
+                                // Halaman 1: Form Login
+                                LoginForm(),
+
+                                // Halaman 2: Form Registrasi
+                                RegisterForm(),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
